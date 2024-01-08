2da8fb100105e46c298c0e2a23054fb5f9d05426
keji <tomitoyo.93@gmail.com>
2024-01-08 23:13:50 +0100

    images import

---
+++ assets/images/index.js
@@ -0,0 +1,12 @@
+import authImage from './authImage.png';
+import berry from './berry.png';
+import customer from './customer.png';
+import delivery from './delivery.png';
+import ellipse from './Ellipse.png';
+import fruit from './fruitSalad.png';
+import honey from './honey.png';
+import order from './order.png';
+import quiona from './quiona.png';
+import welcome from './welcome';
+
+export { authImage, berry, customer, delivery, ellipse, fruit,honey, order, quiona, welcome}
