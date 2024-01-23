aa55669336f62d2f4c201ed00466ac4708edb6b2
keji <tomitoyo.93@gmail.com>
2024-01-23 22:30:47 +0100

    foodApp setput

--- assets/images/index.js
+++ assets/images/index.js
@@ -8,5 +8,14 @@ import honey from './honey.png';
 import order from './order.png';
 import quiona from './quiona.png';
 import Welcome from './welcome.png';
+import love from './love.png';
+import cart from './cart.png';
+import loveGroup from './loveGroup.png';
+import menu from './menu.png';
+import minus from './minus.png';
+import plus from './cross.png';
+import search from './search.png';
+import orderTaken from './orderTaken.png';
+import biSearch from './bi_search.png';
 
-export { authImage, berry, customer, delivery, ellipse, fruit,honey, order, quiona, Welcome}
+export { authImage, berry, customer, delivery, ellipse, fruit,honey, order, quiona, Welcome, love, cart, loveGroup, menu, minus, plus, search, orderTaken, biSearch }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,73 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native'
+import { cart, menu, search, biSearch } from '../assets/images';
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <View style={{flexDirection: 'row', gap: 50, paddingTop: 40}}>
+        <View style={{flexDirection: 'row', alignItems: 'center', gap: 16, marginTop: '-17%'}}>
+          <Image source={menu}/>
+          <Text style={{fontSize: 16, fontWeight: 400, paddingVertical: 10, color: '#27214D'}}>Welcome, Chris.</Text>
+        </View>
+        <View>
+        <Image source={cart} resizeMode='contain'/>
+        </View>
+        </View>
+        <View style={{flex: 0.5, justifyContent: 'center', alignItems: 'center', width: '100%', flexDirection: 'row', gap: 10}}>
+          <View style={styles.input}>
+            <Image source={biSearch}/>
+          <TextInput placeholder='Chris' />
+          </View>
+        <Image source={search}/>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    
+  },
+  image: {
+      backgroundColor: '#FFA451',
+      width: '100%',
+      alignItems: 'center',
+      paddingTop: 120,
+      paddingBottom: 30,
+
+  }, 
+  img: {
+     marginBottom: 10, 
+  }, 
+  body: {
+      backgroundColor: '#fff',
+      width: '100%',
+      paddingHorizontal: 24,
+      alignItems: 'center',
+
+  },
+  input: {
+      width: '90%',
+      height: 56,
+      borderRadius: 10,
+      borderColor: 'transparent',
+      backgroundColor: '#F7F5F5',
+      paddingLeft: 10,
+      color: '#1D1C1A',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      flexDirection: 'row',
+      gap: 10,
+
+  }
+
+});
 
 export default HomeScreen
