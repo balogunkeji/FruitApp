5344352f5ae04c2570ab6a2aed719cf79be7a2ce
keji <tomitoyo.93@gmail.com>
2024-02-01 11:49:05 +0100

    foodApp setput

--- screens/TrackOrder.js
+++ screens/TrackOrder.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Text, View, StyleSheet, Image } from 'react-native'
 import { orderTaken } from '../assets/images';
 import { Button } from '../components/Button';
+import LottieView from 'lottie-react-native';
+
 
 const TrackOrder = ({navigation}) => {
+  const animation = useRef(null);
   return (
     <View style={styles.container}>
-        <Image source={orderTaken}/>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 300,
+          height: 300,
+        }}
+        source={require('../assets/lottie/order.json')}
+      />
         <View style={{marginVertical: 30, justifyContent: 'center', gap: 10}}>
             <Text style={{fontSize: 24, fontWeight: 500, color: 'rgba(39, 33, 77, 1)', textAlign: 'center'}}>Order Taken</Text>
             <Text style={{fontSize: 16, fontWeight: 300, color: 'rgba(17, 17, 17, 1)', textAlign: 'center', width: 250}}>Your order have been taken and is being attended to</Text>
